373b723ec1ebdb1e2f163ae7add5b62a6fa47914
Nam Truong Le <nam.truong.le@sap.com>
2022-12-11 07:16:24 +0100

    fixed SSM parameter name for database

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.0.4] - 2022-12-11
+### Fixed
+- SSM parameter name for database
+
 ## [1.0.3] - 2022-12-10
 ### Fixed
 - Go mod tidy
--- pkg/db/store.go
+++ pkg/db/store.go
@@ -14,7 +14,7 @@ import (
 const colStores = "stores"
 
 func CollectionStores(ctx context.Context) (*mongo.Collection, error) {
-	database, err := ssm.GetParameter(ctx, "/mongo/database", false)
+	database, err := ssm.GetParameter(ctx, "/mongo/db", false)
 	if err != nil {
 		return nil, err
 	}
