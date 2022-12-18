3f41b2a5b4356aaeccc356b9a4fa4c6ee129317a
Nam Truong Le <nam.truong.le@sap.com>
2022-12-18 08:17:09 +0100

    feature: add order create model

---
+++ pkg/admin/orders_create.go
@@ -0,0 +1,62 @@
+package admin
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+
+	"github.com/asia-loop-gmbh/asia-loop-utils-go/pkg/db"
+)
+
+type CreateOrderRequest struct {
+	FirstName          string                 `json:"firstName"`
+	LastName           string                 `json:"lastName"`
+	Telephone          string                 `json:"telephone"`
+	Email              string                 `json:"email"`
+	AddressLine1       string                 `json:"addressLine1"`
+	AddressLine2       string                 `json:"addressLine2"`
+	Postcode           string                 `json:"postcode"`
+	City               string                 `json:"city"`
+	ShippingMethod     db.OrderShippingMethod `json:"shippingMethod"`
+	Items              []db.OrderItem         `json:"items"`
+	Net                string                 `json:"net"`
+	Tax                string                 `json:"tax"`
+	Total              string                 `json:"total"`
+	CouponCode         string                 `json:"couponCode"`
+	AppliedCouponNet   string                 `json:"appliedCouponNet"`
+	AppliedCouponTax   string                 `json:"appliedCouponTax"`
+	AppliedCouponTotal string                 `json:"appliedCouponTotal"`
+	Store              primitive.ObjectID     `json:"store"`
+	CompanyKey         string                 `json:"companyKey"`
+}
+
+type CreateOrderAddressOptions struct {
+	FirstName    string
+	LastName     string
+	Telephone    string
+	Email        string
+	AddressLine1 string
+	AddressLine2 string
+	Postcode     string
+	City         string
+}
+
+type CreateOrderOrderOptions struct {
+	OrderID            string
+	OrderNumber        string
+	InvoiceNumber      string
+	Items              []db.OrderItem
+	DeliveryDate       string
+	DeliveryTime       string
+	Net                string
+	Total              string
+	Tax                string
+	Tip                string
+	CouponCode         string
+	AppliedCouponNet   string
+	AppliedCouponTax   string
+	AppliedCouponTotal string
+	ShippingMethod     db.OrderShippingMethod
+	CustomerNote       string
+	Status             db.OrderStatus
+	Store              primitive.ObjectID
+	CompanyKey         string
+}
