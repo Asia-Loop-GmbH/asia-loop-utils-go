8f70e72e1b11270bcd0e588b5992123c02d005ea
Nam Truong Le <nam.truong.le@sap.com>
2022-12-19 07:13:29 +0100

    added db global config

---
+++ pkg/db/global-config.go
@@ -0,0 +1,28 @@
+package db
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/nam-truong-le/lambda-utils-go/pkg/aws/ssm"
+	"github.com/nam-truong-le/lambda-utils-go/pkg/mongodb"
+)
+
+const colGlobalConfig = "globalconfigs"
+
+func CollectionGlobalConfig(ctx context.Context) (*mongo.Collection, error) {
+	database, err := ssm.GetParameter(ctx, "/mongo/database", false)
+	if err != nil {
+		return nil, err
+	}
+	return mongodb.Collection(ctx, database, colGlobalConfig)
+}
+
+type GlobalConfig struct {
+	ID                           primitive.ObjectID `bson:"_id" json:"id"`
+	ProductAttributeOutOfStockIn int                `bson:"productAttributeOutOfStockInId" json:"productAttributeOutOfStockInId"`
+	ProductAttributePfandId      int                `bson:"productAttributePfandId" json:"productAttributePfandId"`
+	PusherAPIKey                 string             `json:"pusherApiKey"`
+}
