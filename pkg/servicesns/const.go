c5df2a11430b63acce352c3f98443fb92f724a82
Nam Truong Le <nam.truong.le@sap.com>
2022-12-19 05:57:19 +0100

    add SSM parameters const

---
+++ pkg/servicesns/const.go
@@ -0,0 +1,9 @@
+package servicesns
+
+const (
+	TopicOrderCreatedARN   = "/sns/createdOrder/arn"
+	TopicOrderFinalizedARN = "/sns/order/finalized/arn"
+	TopicNewWooOrderARN    = "/sns/woo/order/new"
+	TopicPickupReadyARN    = "/sns/order/pickup-ready/arn"
+	TopicPOSInvoiceARN     = "/sns/order/pos-invoice/arn"
+)
---
+++ pkg/servicesqs/const.go
@@ -0,0 +1,7 @@
+package servicesqs
+
+const (
+	QueueSendEmailOrderCreatedARN = "/sqs/sendEmail/orderCreated/arn"
+	QueueSendEmailPickupARN       = "/sqs/sendEmail/orderPickupReady/arn"
+	QueueSendEmailPOSInvoiceARN   = "/sqs/sendEmail/orderPosInvoice/arn"
+)
