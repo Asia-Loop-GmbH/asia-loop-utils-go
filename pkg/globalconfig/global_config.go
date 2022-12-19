bcd4e95b612a1180ca4329f11591224e1569c8f6
Nam Truong Le <nam.truong.le@sap.com>
2022-12-19 15:06:47 +0100

    feature: add package globalconfig

---
+++ pkg/globalconfig/global_config.go
@@ -0,0 +1,23 @@
+package globalconfig
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/asia-loop-gmbh/asia-loop-utils-go/pkg/db"
+)
+
+func Get(ctx context.Context) (*db.GlobalConfig, error) {
+	colGlbCfg, err := db.CollectionGlobalConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+	find := colGlbCfg.FindOne(context.Background(), bson.M{})
+	globalConfig := new(db.GlobalConfig)
+	err = find.Decode(globalConfig)
+	if err != nil {
+		return nil, err
+	}
+	return globalConfig, nil
+}
---
+++ pkg/globalconfig/global_config_test.go
@@ -0,0 +1,23 @@
+package globalconfig_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/asia-loop-gmbh/asia-loop-utils-go/pkg/globalconfig"
+	"github.com/nam-truong-le/lambda-utils-go/pkg/logger"
+)
+
+func TestGet(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	ctx := context.Background()
+	log := logger.FromContext(ctx)
+
+	cfg, err := globalconfig.Get(ctx)
+	assert.NoError(t, err)
+	log.Infof("%v", cfg)
+}
