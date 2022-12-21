d2dc371239865177d5fb1055a1a1775b2bcd61f8
Nam Truong Le <nam.truong.le@sap.com>
2022-12-21 06:48:24 +0100

    fix missing property InvoicePDFBase64

--- pkg/api/order_public.go
+++ pkg/api/order_public.go
@@ -9,19 +9,20 @@ import (
 )
 
 type OrderPublic struct {
-	ID            primitive.ObjectID `json:"id"`
-	OrderID       string             `json:"orderId"`
-	OrderNumber   string             `json:"orderNumber"`
-	InvoiceNumber string             `json:"invoiceNumber"`
-	Address       string             `json:"address"`
-	AddressLine2  string             `json:"addressLine2"`
-	DeliveryDate  string             `json:"deliveryDate"`
-	DeliveryTime  string             `json:"deliveryTime"`
-	Status        db.OrderStatus     `json:"status"`
-	Printed       bool               `json:"printed"`
-	CreatedAt     time.Time          `json:"createdAt"`
-	GroupKey      string             `json:"groupKey"`
-	GroupPosition int                `json:"groupPosition"`
-	DriverName    string             `json:"driverName"`
-	Invoice       string             `json:"invoice"`
+	ID               primitive.ObjectID `json:"id"`
+	OrderID          string             `json:"orderId"`
+	OrderNumber      string             `json:"orderNumber"`
+	InvoiceNumber    string             `json:"invoiceNumber"`
+	Address          string             `json:"address"`
+	AddressLine2     string             `json:"addressLine2"`
+	DeliveryDate     string             `json:"deliveryDate"`
+	DeliveryTime     string             `json:"deliveryTime"`
+	Status           db.OrderStatus     `json:"status"`
+	Printed          bool               `json:"printed"`
+	CreatedAt        time.Time          `json:"createdAt"`
+	GroupKey         string             `json:"groupKey"`
+	GroupPosition    int                `json:"groupPosition"`
+	DriverName       string             `json:"driverName"`
+	Invoice          string             `json:"invoice"`
+	InvoicePDFBase64 string             `json:"invoicePDFBase64"`
 }
