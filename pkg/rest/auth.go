f59d01dd4e0e38b18dff14d7acb480bbd468928c
Nam Truong Le <nam.truong.le@sap.com>
2023-01-11 17:20:50 +0100

    fix: log more info if user not found

--- pkg/rest/auth.go
+++ pkg/rest/auth.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/samber/lo"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/asia-loop-gmbh/asia-loop-utils-go/v2/pkg/shop/db"
 	"github.com/nam-truong-le/lambda-utils-go/v3/pkg/logger"
@@ -27,15 +28,20 @@ func RequireAdmin(ctx context.Context, request *events.APIGatewayProxyRequest) *
 	find := colRoles.FindOne(ctx, bson.M{"user": username})
 	user := new(db.User)
 	err = find.Decode(user)
+	if err == mongo.ErrNoDocuments {
+		log.Infof("%+v", request.RequestContext.Identity)
+		return rest.ResponseError(ctx, http.StatusForbidden, request, errors.Wrapf(err, "user [%s] not found", username))
+	}
 	if err != nil {
-		return rest.ResponseError(ctx, http.StatusInternalServerError, request, errors.Wrap(err, "failed to find user"))
+		return rest.ResponseError(ctx, http.StatusInternalServerError, request, errors.Wrapf(err, "failed to find user [%s]", username))
 	}
 
 	if !lo.ContainsBy(user.Roles, func(item db.RoleEntry) bool {
 		return item.Name == db.RoleAdmin
 	}) {
+		log.Infof("%+v", request.RequestContext.Identity)
 		log.Errorf("User [%s] doesn't have admin role", username)
-		return rest.Response(ctx, http.StatusUnauthorized, request, nil)
+		return rest.Response(ctx, http.StatusForbidden, request, nil)
 	}
 
 	return nil
