5aca7fbb389f65558ae07d9979d753f7f33a4fc4
Nam Truong Le <nam.truong.le@sap.com>
2023-01-17 17:03:48 +0100

    fix: cart models

--- pkg/shop/cart/cart.go
+++ pkg/shop/cart/cart.go
@@ -14,7 +14,7 @@ import (
 type PublicCart struct {
 	ID        primitive.ObjectID `json:"id"`
 	Items     []PublicCartItem   `json:"items"`
-	Summary   *PublicCartSummary `json:"summary,omitempty"`
+	Summary   PublicCartSummary  `json:"summary"`
 	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
 	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
@@ -51,6 +51,14 @@ func Calculate(ctx context.Context, c *db.Cart) (*PublicCart, error) {
 				TaxClass:  "takeaway",
 			}
 		}),
+		Summary: PublicCartSummary{
+			Total:    "0.00",
+			TotalTax: "0.00",
+			TotalNet: "0.00",
+			Taxes: map[string]string{
+				"MwSt. 7%": "0.00",
+			},
+		},
 		CreatedAt: c.CreatedAt,
 		UpdatedAt: c.UpdatedAt,
 	}, nil
