5a83d882419a5d666ce6dff0c3cfac4f29196bf9
Nam Truong Le <nam.truong.le@sap.com>
2023-01-21 06:03:13 +0100

    breaking: bump v5

--- pkg/normalizer/normalizer_test.go
+++ pkg/normalizer/normalizer_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/asia-loop-gmbh/asia-loop-utils-go/v4/pkg/normalizer"
+	"github.com/asia-loop-gmbh/asia-loop-utils-go/v5/pkg/normalizer"
 )
 
 func TestEmail(t *testing.T) {
--- pkg/servicecognito/get_test.go
+++ pkg/servicecognito/get_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/asia-loop-gmbh/asia-loop-utils-go/v4/pkg/servicecognito"
+	"github.com/asia-loop-gmbh/asia-loop-utils-go/v5/pkg/servicecognito"
 	commoncontext "github.com/nam-truong-le/lambda-utils-go/v3/pkg/context"
 )
 
