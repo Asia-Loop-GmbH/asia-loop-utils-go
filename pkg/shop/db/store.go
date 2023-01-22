aa51aa5ef5436ea054b5ebe32347bee709a1e3fd
Nam Truong Le <nam.truong.le@sap.com>
2023-01-22 08:45:18 +0100

    feature: get slots from store

--- pkg/shop/db/store.go
+++ pkg/shop/db/store.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"time"
 
+	"github.com/samber/lo"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 
@@ -33,6 +34,24 @@ type Store struct {
 	TaxNumber            string             `bson:"taxNumber" json:"taxNumber"`
 	MBW                  map[string]string  `bson:"mbw" json:"mbw"`
 	Slots                []map[string]bool  `bson:"slots" json:"slots"` // week starts with Sunday = index 0
+	Holidays             []string           `bson:"holidays" json:"holidays"`
 	CreatedAt            time.Time          `bson:"createdAt" json:"createdAt"`
 	UpdatedAt            time.Time          `bson:"updatedAt" json:"updatedAt"`
+}
+
+func (s *Store) GetSlots(date string) []string {
+	if lo.Contains(s.Holidays, date) {
+		return make([]string, 0)
+	}
+
+	d, err := time.Parse("2006-01-02", date)
+	if err != nil {
+		return make([]string, 0)
+	}
+
+	weekDay := int(d.Weekday())
+	slots := s.Slots[weekDay]
+	return lo.Filter(lo.Keys(slots), func(item string, index int) bool {
+		return slots[item]
+	})
 }
