419160cae366d8a74a6b4c307e7d61af33198666
Nam Truong Le <lenamtruong@gmail.com>
2023-01-23 07:27:18 +0100

    feature: add coupon model (#75)
    Co-authored-by: Nam Truong Le <nam.truong.le@sap.com>

---
+++ pkg/shop/db/coupons.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/nam-truong-le/lambda-utils-go/v3/pkg/aws/ssm"
+	"github.com/nam-truong-le/lambda-utils-go/v3/pkg/mongodb"
+)
+
+const colCoupons = "coupons"
+
+func CollectionCoupons(ctx context.Context) (*mongo.Collection, error) {
+	database, err := ssm.GetParameter(ctx, "/shop/mongo/database", false)
+	if err != nil {
+		return nil, err
+	}
+	return mongodb.Collection(ctx, database, colCoupons)
+}
+
+const (
+	CouponTypeGiftCard = "GiftCard"
+)
+
+type Coupon struct {
+	ID        primitive.ObjectID `bson:"_id" json:"id"`
+	Type      string             `bson:"type" json:"type"`
+	Code      string             `bson:"code" json:"code"`
+	Total     string             `bson:"total" json:"total"`
+	Usage     []CouponUsage      `bson:"usage" json:"usage"`
+	Disabled  bool               `bson:"disabled" json:"disabled"`
+	CreatedAt time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
+}
+
+type CouponUsage struct {
+	OrderID   string    `bson:"orderId" json:"orderId"`
+	Total     string    `bson:"total" json:"total"`
+	CreatedAt time.Time `bson:"createdAt" json:"createdAt"`
+}
