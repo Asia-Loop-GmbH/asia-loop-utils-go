52b2258cc02b69a42a39479511622417fb1f3cb5
Nam Truong Le <lenamtruong@gmail.com>
2023-10-29 22:16:48 +0100

    feature: add tax configuration to db.Product

--- pkg/db/products.go
+++ pkg/db/products.go
@@ -28,6 +28,14 @@ const (
 	ProductTypeVariable ProductType = "variable"
 )
 
+type TaxClass string
+
+const (
+	TaxClassStandard TaxClass = "standard"
+	TaxClassReduced  TaxClass = "mitnehmen"
+	TaxClassZero     TaxClass = "steuerfreie"
+)
+
 type ProductAttribute struct {
 	WPID      int      `bson:"id" json:"wpId"` // it's ok in patch request because we won't change this
 	Name      string   `bson:"name" json:"name"`
@@ -61,13 +69,16 @@ type Product struct {
 	Price        string             `bson:"price" json:"price"`
 	RegularPrice string             `bson:"regularPrice" json:"regularPrice"`
 	SalePrice    string             `bson:"salePrice" json:"salePrice"`
-	TaxClass     string             `bson:"taxClass" json:"taxClass"`
-	Categories   []string           `bson:"categories" json:"categories"`
-	Images       []string           `bson:"images" json:"images"`
-	Attributes   []ProductAttribute `bson:"attributes" json:"attributes"`
-	Variations   []ProductVariation `bson:"variations" json:"variations"`
-	OutOfStockIn []string           `bson:"outOfStockIn" json:"outOfStockIn"`
-	Pfand        string             `bson:"pfand" json:"pfand"`
-	CreatedAt    time.Time          `bson:"createdAt" json:"createdAt"`
-	UpdatedAt    time.Time          `bson:"updatedAt" json:"updatedAt"`
+	// TaxClass is deprecated
+	TaxClass          string             `bson:"taxClass" json:"taxClass"`
+	TaxClassImHaus    TaxClass           `bson:"taxClassImHaus" json:"taxClassImHaus"`
+	TaxClassMitnehmen TaxClass           `bson:"taxClassMitnehmen" json:"taxClassMitnehmen"`
+	Categories        []string           `bson:"categories" json:"categories"`
+	Images            []string           `bson:"images" json:"images"`
+	Attributes        []ProductAttribute `bson:"attributes" json:"attributes"`
+	Variations        []ProductVariation `bson:"variations" json:"variations"`
+	OutOfStockIn      []string           `bson:"outOfStockIn" json:"outOfStockIn"`
+	Pfand             string             `bson:"pfand" json:"pfand"`
+	CreatedAt         time.Time          `bson:"createdAt" json:"createdAt"`
+	UpdatedAt         time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
