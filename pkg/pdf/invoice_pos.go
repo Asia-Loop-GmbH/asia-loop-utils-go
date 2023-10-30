df4c90366feff8e1fffd797d241bb300a0c0c050
Nam Truong Le <lenamtruong@gmail.com>
2023-10-30 23:55:15 +0100

    fix: correct tax info in admin pdf invoice (#366)

--- pkg/pdf/invoice_pos.go
+++ pkg/pdf/invoice_pos.go
@@ -9,15 +9,47 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/samber/lo"
+	"github.com/shopspring/decimal"
 
 	"github.com/asia-loop-gmbh/asia-loop-utils-go/v8/pkg/db"
 	"github.com/nam-truong-le/lambda-utils-go/v4/pkg/logger"
 )
 
+func adminTaxClassToText(taxClass db.TaxClass) string {
+	switch taxClass {
+	case db.TaxClassStandard:
+		return "19%"
+	case db.TaxClassReduced:
+		return "7%"
+	default:
+		return "N/A"
+	}
+}
+
 func InvoiceFromAdminOrder(ctx context.Context, order *db.Order, store *db.Store, customer *db.Customer) ([]byte, error) {
 	log := logger.FromContext(ctx)
 	log.Infof("Generate invoice for admin order %s", order.OrderNumber)
 
+	tax19 := decimal.Zero
+	tax7 := decimal.Zero
+	items := lo.Map(order.Items, func(item db.OrderItem, _ int) invoiceTemplateItems {
+		if item.TaxClass == db.TaxClassReduced {
+			tax7 = tax7.Add(decimal.RequireFromString(item.Tax))
+		}
+
+		if item.TaxClass == db.TaxClassStandard {
+			tax19 = tax19.Add(decimal.RequireFromString(item.Tax))
+		}
+
+		return invoiceTemplateItems{
+			Name:     item.Name,
+			SKU:      item.SKU,
+			Amount:   item.Quantity,
+			Total:    item.Total,
+			TaxClass: adminTaxClassToText(item.TaxClass),
+			Tax:      item.Tax,
+		}
+	})
 	props := invoiceTemplateProps{
 		StoreName:       store.Name,
 		StoreAddress:    store.Address,
@@ -29,16 +61,11 @@ func InvoiceFromAdminOrder(ctx context.Context, order *db.Order, store *db.Store
 		InvoiceNumber:   order.InvoiceNumber,
 		OrderNumber:     order.OrderNumber,
 		Date:            order.UpdatedAt,
-		Items: lo.Map(order.Items, func(item db.OrderItem, _ int) invoiceTemplateItems {
-			return invoiceTemplateItems{
-				Name:   item.Name,
-				SKU:    item.SKU,
-				Amount: item.Quantity,
-				Total:  item.Total,
-			}
-		}),
-		Total: order.Total,
-		Tax:   order.Tax,
+		Items:           items,
+		Total:           order.Total,
+		Tax:             order.Tax,
+		Tax19:           tax19.StringFixed(2),
+		Tax7:            tax7.StringFixed(2),
 	}
 	t, err := template.New("invoice").Funcs(
 		template.FuncMap{
