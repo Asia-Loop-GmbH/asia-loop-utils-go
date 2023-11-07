ab386164cd0c0bfe571306205006e62b2d97c48c
Nam Truong Le <nam.truong.le@sap.com>
2023-11-07 23:07:39 +0100

    feature: add store deliverectIntegration

--- pkg/shop/db/store.go
+++ pkg/shop/db/store.go
@@ -22,23 +22,29 @@ func CollectionStores(ctx context.Context) (*mongo.Collection, error) {
 	return mongodb.Collection(ctx, database, colStores)
 }
 
+type DeliverectIntegration struct {
+	AccountID  string `bson:"accountId" json:"accountId"`
+	LocationID string `bson:"locationId" json:"locationId"`
+}
+
 type Store struct {
-	ID                   primitive.ObjectID         `bson:"_id" json:"id"`
-	Email                string                     `bson:"email" json:"email"`
-	Telephone            string                     `bson:"telephone" json:"telephone"`
-	Name                 string                     `bson:"name" json:"name"`
-	Key                  string                     `bson:"key" json:"key"`
-	Address              string                     `bson:"address" json:"address"`
-	Owner                string                     `bson:"owner" json:"owner"`
-	BusinessRegistration string                     `bson:"businessRegistration" json:"businessRegistration"`
-	TaxNumber            string                     `bson:"taxNumber" json:"taxNumber"`
-	MBW                  map[string]string          `bson:"mbw" json:"mbw"`
-	MBWAllowOnlyCities   map[string][]string        `bson:"mbwAllowOnlyCities" json:"mbwAllowOnlyCities,omitempty"`
-	Slots                []map[string]bool          `bson:"slots" json:"slots"` // week starts with Sunday = index 0
-	Holidays             []string                   `bson:"holidays" json:"holidays"`
-	SpecialDays          map[string]map[string]bool `bson:"specialDays,omitempty" json:"specialDays,omitempty"`
-	CreatedAt            time.Time                  `bson:"createdAt" json:"createdAt"`
-	UpdatedAt            time.Time                  `bson:"updatedAt" json:"updatedAt"`
+	ID                    primitive.ObjectID         `bson:"_id" json:"id"`
+	Email                 string                     `bson:"email" json:"email"`
+	Telephone             string                     `bson:"telephone" json:"telephone"`
+	Name                  string                     `bson:"name" json:"name"`
+	Key                   string                     `bson:"key" json:"key"`
+	Address               string                     `bson:"address" json:"address"`
+	Owner                 string                     `bson:"owner" json:"owner"`
+	BusinessRegistration  string                     `bson:"businessRegistration" json:"businessRegistration"`
+	TaxNumber             string                     `bson:"taxNumber" json:"taxNumber"`
+	MBW                   map[string]string          `bson:"mbw" json:"mbw"`
+	MBWAllowOnlyCities    map[string][]string        `bson:"mbwAllowOnlyCities" json:"mbwAllowOnlyCities,omitempty"`
+	Slots                 []map[string]bool          `bson:"slots" json:"slots"` // week starts with Sunday = index 0
+	Holidays              []string                   `bson:"holidays" json:"holidays"`
+	SpecialDays           map[string]map[string]bool `bson:"specialDays,omitempty" json:"specialDays,omitempty"`
+	CreatedAt             time.Time                  `bson:"createdAt" json:"createdAt"`
+	UpdatedAt             time.Time                  `bson:"updatedAt" json:"updatedAt"`
+	DeliverectIntegration *DeliverectIntegration     `bson:"deliverectIntegration,omitempty" json:"deliverectIntegration,omitempty"`
 }
 
 func (s *Store) GetSlots(date string) []string {
